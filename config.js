b785a07f887cf8c3dee48c256e6e4c4a9327bcf3
c01067000172-cell <c01067000172@gmail.com>
2026-09-12 00:13:46 +0900

    Add WHENG public configuration

---
+++ config.js
@@ -0,0 +1,9 @@
+window.WHENG_CONFIG = {
+  supabaseUrl: 'https://bcjhwlqvytphupxpzjlj.supabase.co',
+  supabasePublishableKey: 'sb_publishable_uPxC7lYV8hxyMP9Q0T5z_Q_BCTc6LUE',
+  phoneDisplay: '010-0000-0000',
+  phoneTel: '01000000000',
+  kakaoUrl: '',
+  businessName: 'WHENG 생활설비',
+  serviceAreas: '수원 · 화성 · 용인 · 오산 외 협의'
+};
